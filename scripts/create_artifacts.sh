70158b83438624edd3d40accc77545e71870987a
Eduard Jacobs <edoter92@gmail.com>
2021-02-21 00:14:45 -0800

    create artifacts script

---
+++ scripts/create_artifacts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e 
+
+# Replace Environmental Variables 
+# ($AWS_DEFAULT_REGION) with acutal env passed as arg
+
+#============= Environmental Variables =============
+export AWS_ACCOUNT_ID=$1
+export AWS_DEFAULT_REGION=$2
+export CONTAINER_NAME="MyApp-Task-Def"
+export SERVICE_NAME="MyApp-Service"
+export CONTAINER_PORT=80
+
+
+#============ Template and Output file =============
+all_templates=( 
+"appSpec.yml"
+"taskdef.json"
+)
+
+#============ Create File Replacing ENVs=============
+for i in "${all_templates[@]}"
+do
+  finalFile=$i
+  template="./aws.templates/${i}"
+
+  rm -f ${finalFile}
+  ( echo "cat <<EOF >${finalFile}";
+    cat ${template};
+  ) >${finalFile}
+  . ${finalFile}
+done
+
+
--- scripts/prod_deploy.sh
+++
@@ -1,3 +0,0 @@
-import os, sys
-
-os.system('node ./scripts/artifacts.js {} {}'.format(sys.argv[1], sys.argv[2]))
