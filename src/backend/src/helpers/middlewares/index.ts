63e8ed7ca623c2dfa92b0fd650bec4b4a88acbdf
Eduard Jacobs <edote92@gmail.com>
2021-02-21 22:21:18 -0800

    backend-> middleware types fix

--- src/backend/src/helpers/middlewares/index.ts
+++ src/backend/src/helpers/middlewares/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
-export const headers = (app: express.Application) => {
-  app.use((_: any, res: any, next: any) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-  });
+export const headers = (app: express.Application): void => {
+  app.use(
+    (_: express.Request, res: express.Response, next: express.NextFunction) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      );
+      next();
+    }
+  );
 };
