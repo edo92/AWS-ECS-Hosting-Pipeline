e27be4f3bed8fc68bf6fcbc2445ae008cb8eda8a
cyber-netics <67401984+cyber-netics@users.noreply.github.com>
2021-02-24 22:24:26 -0800

    middleware fixed (#13)

--- src/backend/src/helpers/middlewares/index.ts
+++ src/backend/src/helpers/middlewares/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 
-export const headers = (app: express.Application): void => {
-  app.use(
-    (_: express.Request, res: express.Response, next: express.NextFunction) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-      );
-      next();
-    }
+type Req = express.Request;
+type Res = express.Response;
+type Next = express.NextFunction;
+
+const headers = (_: Req, res: Res, next: Next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
+
+  next();
 };
+
+export default headers;
